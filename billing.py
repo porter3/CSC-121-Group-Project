d48d581c00f50c7a6196b6b57e8186365b579f57
prenticem7673 <78758149+prenticem7673@users.noreply.github.com>
2021-11-29 16:56:23 -0500

    Update billing.py

--- billing.py
+++ billing.py
@@ -1,4 +1,3 @@
-# ----------------------------------------------------------------
 # Author:
 # Date:
 #
@@ -6,7 +5,7 @@
 # for students in the class registration system.  Student and
 # class records are reviewed and tuition fees are calculated.
 # -----------------------------------------------------------------
-def calculate_hours_and_bill(id, c_rosters, c_hours, s_in_state=True):
+def calculate_hours_and_bill(id, c_rosters, c_hours, s_in_state):
     # ------------------------------------------------------------
     # This function calculate billing information. It takes four
     # parameters: id, the student id; s_in_state, the list of
@@ -15,12 +14,20 @@ def calculate_hours_and_bill(id, c_rosters, c_hours, s_in_state=True):
     # This function returns the number of course hours and tuition
     # cost.
     # ------------------------------------------------------------
-    if s_in_state:
-        billing = c_hours * float(225.00)
-        return c_hours and billing
-    else:
-        billing = c_hours * float(850.00)
-        return c_hours and billing
+   for course in c_rosters:
+    for s_id in c_rosters[course]:
+        if str(id) in s_id:
+            # Now what? Should this be wrapped in the for blocks below?
+
+      for ele in s_in_state:
+       if s_in_state[ele]:
+           # How do I associate the ID to courses and course hours?
+           billing = c_hours * float(225.00)
+           return c_hours and billing
+       if not s_in_state[ele]:
+           # How do I associate the ID to courses and course hours?
+            billing = c_hours * float(850.00)
+            return c_hours and billing
 
 
 def display_hours_and_bill(hours, cost):
@@ -29,6 +36,6 @@ def display_hours_and_bill(hours, cost):
     # is taking and the total tuition cost. It takes two parameters:
     # hours and cost. This function has no return value.
     # ------------------------------------------------------------
-    display = hours, float(cost)
-    print(display)
+    print(str(id), int(hours), float(cost))
+
 
